71819beab02a3e47bf1050f5e101f2262802fa4c
Jay <92765430+regulating@users.noreply.github.com>
2024-07-03 22:20:03 +0000

    Update Loading screen
    Added Fade

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
     setTimeout(function () {
         const loadingScreen = document.getElementById('loading-screen');
-        loadingScreen.style.display = 'none';
+        loadingScreen.classList.add('hidden');  
     }, 1500); // 1.5 sec
 });
