837cd05b474f945aa40310fd93bc95c1feb08f12
Jay <92765430+regulating@users.noreply.github.com>
2024-07-03 22:32:59 +0000

    Typing text + Misc Changes

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-window.addEventListener('load', function () {
-    setTimeout(function () {
-        const loadingScreen = document.getElementById('loading-screen');
-        loadingScreen.classList.add('hidden');  
-    }, 1500); // 1.5 sec
+document.addEventListener('DOMContentLoaded', function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    setTimeout(() => {
+        loadingScreen.classList.add('hidden');
+    }, 1500);
+
+
+    const typingText = `const portfolio = new Portfolio();
+portfolio.display();`;
+
+    let index = 0;
+    function type() {
+        if (index < typingText.length) {
+            document.querySelector('.typing-animation').textContent += typingText.charAt(index);
+            index++;
+            setTimeout(type, 100);                                  
+        }
+    }
+    setTimeout(type, 3500); 
 });
