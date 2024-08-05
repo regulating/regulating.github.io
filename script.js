a2af5faaddd80a2fdb18a0fa372558ed8687db87
Jay <92765430+regulating@users.noreply.github.com>
2024-08-05 18:33:50 +0100

    Design Changes
    Alternate layout introduced due to the annoying nature of what it used to be XD

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#typed', {
+        strings: ["Developer", "DJ", "Designer"],
+        typeSpeed: 80,
+        backSpeed: 80,
+        loop: true
+    });
+});
