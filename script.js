74eb26cb0626ca50eba80f5c835101a269dbee92
Jay <92765430+regulating@users.noreply.github.com>
2024-08-20 21:45:43 +0100

    Update typing.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('#typed', {
-        strings: ["Developer", "DJ", "Designer"],
+        strings: ["Jay Fitzgerald", "Developer", "DJ", "Designer"],
+        typeSpeed: 70,
+        backSpeed: 70,
+        loop: true
+    });
+    
+    var subtitleTyped = new Typed('#subtitle-typed', {
+        strings: ["Subtitle 1", "Subtitle 2", "Subtitle 3"],
         typeSpeed: 80,
         backSpeed: 80,
         loop: true
